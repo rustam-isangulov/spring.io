23eef7f458039800a80af8d85420a5bc88f20ea5
rustam-isangulov <rustam.pro@icloud.com>
2022-06-16 21:36:00 +0100

    Initial commit for the elementary project.

---
+++ spring_elementary/src/main/java/models/ClassA.java
@@ -0,0 +1,13 @@
+package models;
+
+public class ClassA {
+    private String name;
+
+    public ClassA(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+}
